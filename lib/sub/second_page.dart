1b6a8776d42ef47cd37fb7f1f769c3d1fa868e24
ppaka <asj0216@gmail.com>
2021-12-10 04:30:29 +0900

    백업

--- lib/sub/second_page.dart
+++ lib/sub/second_page.dart
@@ -35,132 +35,146 @@ class _SecondPageState extends State<SecondPage>
   double progress = 0;
   final urlController = TextEditingController();
 
-  late WebViewController _viewController;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WillPopScope(child: Scaffold(
         body: SafeArea(
             child: Column(children: <Widget>[
-      TextField(
-        decoration: InputDecoration(prefixIcon: Icon(Icons.search)),
-        controller: urlController,
-        keyboardType: TextInputType.url,
-        onSubmitted: (value) {
-          var url = Uri.parse(value);
-          if (url.scheme.isEmpty) {
-            url = Uri.parse("https://arca.live/e/?target=title&keyword=" + value);
-          }
-          webViewController?.loadUrl(urlRequest: URLRequest(url: url));
-        },
-      ),
-      Expanded(
-        child: Stack(
-          children: [
-            InAppWebView(
-              key: webViewKey,
-              initialUrlRequest:
-                  URLRequest(url: Uri.parse("https://arca.live/e/?p=1")),
-              initialOptions: options,
-              pullToRefreshController: pullToRefreshController,
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              onLoadStart: (controller, url) {
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              androidOnPermissionRequest:
-                  (controller, origin, resources) async {
-                return PermissionRequestResponse(
-                    resources: resources,
-                    action: PermissionRequestResponseAction.GRANT);
-              },
-              shouldOverrideUrlLoading: (controller, navigationAction) async {
-                var uri = navigationAction.request.url!;
-
-                if (![
-                  "http",
-                  "https",
-                  "file",
-                  "chrome",
-                  "data",
-                  "javascript",
-                  "about"
-                ].contains(uri.scheme)) {
-                  if (await canLaunch(url)) {
-                    // Launch the App
-                    await launch(
-                      url,
-                    );
-                    // and cancel the request
-                    return NavigationActionPolicy.CANCEL;
+              TextField(
+                decoration: InputDecoration(prefixIcon: Icon(Icons.search)),
+                controller: urlController,
+                keyboardType: TextInputType.url,
+                onSubmitted: (value) {
+                  var url = Uri.parse(value);
+                  if (url.scheme.isEmpty) {
+                    url =
+                        Uri.parse("https://arca.live/e/?target=title&keyword=" + value);
                   }
-                }
+                  webViewController?.loadUrl(urlRequest: URLRequest(url: url));
+                },
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    InAppWebView(
+                      key: webViewKey,
+                      initialUrlRequest:
+                      URLRequest(url: Uri.parse("https://arca.live/e/?p=1")),
+                      initialOptions: options,
+                      pullToRefreshController: pullToRefreshController,
+                      onWebViewCreated: (controller) {
+                        webViewController = controller;
+                      },
+                      onLoadStart: (controller, url) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      androidOnPermissionRequest:
+                          (controller, origin, resources) async {
+                        return PermissionRequestResponse(
+                            resources: resources,
+                            action: PermissionRequestResponseAction.GRANT);
+                      },
+                      shouldOverrideUrlLoading: (controller, navigationAction) async {
+                        var uri = navigationAction.request.url!;
 
-                return NavigationActionPolicy.ALLOW;
-              },
-              onLoadStop: (controller, url) async {
-                pullToRefreshController.endRefreshing();
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              onLoadError: (controller, url, code, message) {
-                pullToRefreshController.endRefreshing();
-              },
-              onProgressChanged: (controller, progress) {
-                if (progress == 100) {
-                  pullToRefreshController.endRefreshing();
-                }
-                setState(() {
-                  this.progress = progress / 100;
-                  urlController.text = this.url;
-                });
-              },
-              onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              onConsoleMessage: (controller, consoleMessage) {
-                print(consoleMessage);
-              },
-            ),
-            progress < 1.0
-                ? LinearProgressIndicator(value: progress)
-                : Container(),
-          ],
-        ),
-      ),
-      ButtonBar(
-        alignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            child: Icon(Icons.arrow_back),
-            onPressed: () {
-              webViewController?.goBack();
-            },
-          ),
-          ElevatedButton(
-            child: Icon(Icons.arrow_forward),
-            onPressed: () {
-              webViewController?.goForward();
-            },
-          ),
-          ElevatedButton(
-            child: Icon(Icons.refresh),
-            onPressed: () {
-              webViewController?.reload();
-            },
-          ),
-        ],
-      ),
-    ])));
+                        if (![
+                          "http",
+                          "https",
+                          "file",
+                          "chrome",
+                          "data",
+                          "javascript",
+                          "about"
+                        ].contains(uri.scheme)) {
+                          if (await canLaunch(url)) {
+                            // Launch the App
+                            await launch(
+                              url,
+                            );
+                            // and cancel the request
+                            return NavigationActionPolicy.CANCEL;
+                          }
+                        }
+
+                        return NavigationActionPolicy.ALLOW;
+                      },
+                      onLoadStop: (controller, url) async {
+                        pullToRefreshController.endRefreshing();
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onLoadError: (controller, url, code, message) {
+                        pullToRefreshController.endRefreshing();
+                      },
+                      onProgressChanged: (controller, progress) {
+                        if (progress == 100) {
+                          pullToRefreshController.endRefreshing();
+                        }
+                        setState(() {
+                          this.progress = progress / 100;
+                          urlController.text = this.url;
+                        });
+                      },
+                      onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onConsoleMessage: (controller, consoleMessage) {
+                        print(consoleMessage);
+                      },
+                    ),
+                    progress < 1.0
+                        ? LinearProgressIndicator(value: progress)
+                        : Container(),
+                  ],
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      webViewController?.goBack();
+                    },
+                  ),
+                  ElevatedButton(
+                    child: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      webViewController?.goForward();
+                    },
+                  ),
+                  ElevatedButton(
+                    child: Icon(Icons.refresh),
+                    onPressed: () {
+                      webViewController?.reload();
+                    },
+                  ),
+                ],
+              ),
+            ]))), onWillPop: () => _goBack(context));
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (webViewController == null) {
+      return true;
+    }
+    if (await webViewController!.canGoBack()) {
+      webViewController!.goBack();
+      return Future.value(false);
+    } else {
+      webViewController!.android.pause();
+      goBack();
+      // return Future.value(true);
+      return false;
+    }
   }
 
   @override
