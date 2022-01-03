21992bd77e905afbaf89b729dcfe9baf9cf813b8
ppaka <asj0216@gmail.com>
2022-01-03 17:09:04 +0900

    알림 표시 문제 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'sub/first_page.dart';
 
 void main() {
