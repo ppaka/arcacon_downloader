792b1cbfe073f423fc77b8a480b59edabe8ab6bf
ppaka <asj0216@gmail.com>
2022-02-06 22:40:42 +0900

    비디오 비활성화

--- lib/sub/arcacon_list.dart
+++ lib/sub/arcacon_list.dart
@@ -1,10 +1,8 @@
-import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart' as dom;
 import 'package:video_player/video_player.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 class ArcaconPage extends StatefulWidget {
   const ArcaconPage({Key? key}) : super(key: key);
@@ -19,7 +17,7 @@ Map<int, VideoPlayerControllerItem> videoControllers = {};
 late Future<List<PreviewArcaconItem>> items;
 
 class VideoPlayerControllerItem {
-  late BetterPlayer player;
+  late VideoPlayerController controller;
   late bool isPlaying;
 }
 
@@ -63,7 +61,8 @@ Future<List<PreviewArcaconItem>> loadPage(bool loadFirstPage) {
   return Future<List<PreviewArcaconItem>>.delayed(const Duration(seconds: 0),
       () async {
     int targetPage = lastLoadPage + 1;
-    String url = "https://arca.live/e/?p={$targetPage}";
+    String url = "https://arca.live/e/?p=$targetPage";
+    print(url);
 
     http.Client client = http.Client();
     http.Response response = await client.get(Uri.parse(url));
@@ -137,60 +136,12 @@ Future<void> requestMore() async {
   return await Future.delayed(const Duration(milliseconds: 1000));
 }
 
-scrollNotification(notification) {
-  // 스크롤 최대 범위
-  /*var containerExtent = notification.metrics.viewportDimension;
-
-  if (notification is ScrollStartNotification) {
-    // 스크롤을 시작하면 발생(손가락으로 리스트를 누르고 움직이려고 할때)
-    // 스크롤 거리값을 0으로 초기화함
-    _dragDistance = 0;
-  } else if (notification is OverscrollNotification) {
-    // 안드로이드에서 동작
-    // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
-    // 스크롤 움직인 만큼 빼준다.(notification.overscroll)
-    _dragDistance -= notification.overscroll;
-  } else if (notification is ScrollUpdateNotification) {
-    // ios에서 동작
-    // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
-    // 스크롤 움직인 만큼 빼준다.(notification.scrollDelta)
-    _dragDistance -= notification.scrollDelta;
-  } else if (notification is ScrollEndNotification) {
-    // 스크롤이 끝났을때 발생(손가락을 리스트에서 움직이다가 뗐을때 발생)
-
-    // 지금까지 움직인 거리를 최대 거리로 나눈다.
-    var percent = _dragDistance / (containerExtent);
-    // 해당 값이 -0.4(40프로 이상) 아래서 위로 움직였다면
-    if (percent <= -0.4) {
-      // maxScrollExtent는 리스트 가장 아래 위치 값
-      // pixels는 현재 위치 값
-      // 두 같이 같다면(스크롤이 가장 아래에 있다)
-      if (notification.metrics.maxScrollExtent ==
-          notification.metrics.pixels) {
-        setState(() {
-          // 서버에서 데이터를 더 가져오는 효과를 주기 위함
-          // 하단에 프로그레스 서클 표시용
-          isMoreRequesting = true;
-        });
-
-        // 서버에서 데이터 가져온다.
-        requestMore().then((value) {
-          setState(() {
-            // 다 가져오면 하단 표시 서클 제거
-            isMoreRequesting = false;
-          });
-        });
-      }
-    }
-  }*/
-}
-
 class _ArcaconListPage extends State<ArcaconPage> {
   Future<void> requestNew() async {
     print('request New');
     previewList.clear();
     videoControllers.forEach((key, value) {
-      value.player.controller.dispose(forceDispose: true);
+      value.controller.dispose();
     });
     videoControllers.clear();
     setState(() {
@@ -210,26 +161,71 @@ class _ArcaconListPage extends State<ArcaconPage> {
     return await Future.delayed(const Duration(milliseconds: 1000));
   }
 
-  Future<BetterPlayer> loadVideo(AsyncSnapshot snapshot, int index) {
-    return Future<BetterPlayer>.delayed(
+  double _dragDistance = 0;
+
+  scrollNotification(notification) {
+    // 스크롤 최대 범위
+    var containerExtent = notification.metrics.viewportDimension;
+
+    if (notification is ScrollStartNotification) {
+      // 스크롤을 시작하면 발생(손가락으로 리스트를 누르고 움직이려고 할때)
+      // 스크롤 거리값을 0으로 초기화함
+      _dragDistance = 0;
+    } else if (notification is OverscrollNotification) {
+      // 안드로이드에서 동작
+      // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
+      // 스크롤 움직인 만큼 빼준다.(notification.overscroll)
+      _dragDistance -= notification.overscroll;
+    } else if (notification is ScrollUpdateNotification) {
+      // ios에서 동작
+      // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
+      // 스크롤 움직인 만큼 빼준다.(notification.scrollDelta)
+      _dragDistance -= notification.scrollDelta!;
+    } else if (notification is ScrollEndNotification) {
+      // 스크롤이 끝났을때 발생(손가락을 리스트에서 움직이다가 뗐을때 발생)
+
+      // 지금까지 움직인 거리를 최대 거리로 나눈다.
+      var percent = _dragDistance / (containerExtent);
+      // 해당 값이 -0.4(40프로 이상) 아래서 위로 움직였다면
+      if (percent <= -0.4) {
+        // maxScrollExtent는 리스트 가장 아래 위치 값
+        // pixels는 현재 위치 값
+        // 두 같이 같다면(스크롤이 가장 아래에 있다)
+        if (notification.metrics.maxScrollExtent ==
+            notification.metrics.pixels) {
+          print("데이터 로드");
+
+          setState(() {
+            // 서버에서 데이터를 더 가져오는 효과를 주기 위함
+            // 하단에 프로그레스 서클 표시용
+            // isMoreRequesting = true;
+          });
+
+          // 서버에서 데이터 가져온다.
+          requestMore().then((value) {
+            setState(() {
+              // 다 가져오면 하단 표시 서클 제거
+              // isMoreRequesting = false;
+            });
+          });
+        }
+      }
+    }
+  }
+
+  Future<VideoPlayerController> loadVideo(AsyncSnapshot snapshot, int index) {
+    return Future<VideoPlayerController>.delayed(
       const Duration(seconds: 0),
       () {
-        /*VideoPlayerController controller = VideoPlayerController.network(
+        VideoPlayerController controller = VideoPlayerController.network(
           snapshot.data![index].imageUrl,
           videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
         );
         controller.initialize();
         controller.setLooping(true);
-        controller.play();*/
+        controller.pause();
 
-        const BetterPlayerBufferingConfiguration(maxBufferMs: 500);
-        var controller = BetterPlayer.network(
-          snapshot.data![index].imageUrl,
-          betterPlayerConfiguration:
-              const BetterPlayerConfiguration(looping: true),
-        );
-        controller.controller.play();
-        videoControllers[index]!.player = controller;
+        videoControllers[index]!.controller = controller;
         videoControllers[index]!.isPlaying = true;
 
         return controller;
@@ -251,32 +247,39 @@ class _ArcaconListPage extends State<ArcaconPage> {
           future: items,
           builder: (context, AsyncSnapshot<List<PreviewArcaconItem>> snapshot) {
             if (snapshot.hasData) {
-              return RefreshIndicator(
-                onRefresh: requestNew,
-                child: GridView.builder(
-                  itemBuilder: (context, position) {
-                    return Card(
-                      child: SizedBox(
-                        height: 10,
-                        width: 50,
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 10.0,
-                            ),
-                            if (snapshot.data![position].imageUrl
-                                .endsWith('mp4'))
-                              SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: FutureBuilder(
-                                  future: loadVideo(snapshot, position),
-                                  builder: (context,
-                                      AsyncSnapshot<BetterPlayer> snapshot) {
-                                    if (snapshot.hasData) {
-                                      print("영상있음");
-                                      return snapshot.data!;
-                                      /*return VisibilityDetector(
+              return NotificationListener<ScrollNotification>(
+                onNotification: (ScrollNotification notification) {
+                  scrollNotification(notification);
+                  return false;
+                },
+                child: RefreshIndicator(
+                  onRefresh: requestNew,
+                  child: GridView.builder(
+                    itemBuilder: (context, position) {
+                      return Card(
+                        child: SizedBox(
+                          height: 10,
+                          width: 50,
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              if (snapshot.data![position].imageUrl
+                                  .endsWith('mp4'))
+                                const SizedBox(
+                                    width: 100,
+                                    height: 100,
+                                    child: /*FutureBuilder(
+                                    future: loadVideo(snapshot, position),
+                                    builder: (context,
+                                        AsyncSnapshot<VideoPlayerController>
+                                            snapshot) {
+                                      if (snapshot.hasData) {
+                                        print("영상있음");
+                                        //return VideoPlayer(snapshot.data!);
+                                        return const CircularProgressIndicator();
+                                        */ /*return VisibilityDetector(
                                           key: UniqueKey(),
                                           child: videoControllers[position]!
                                               .player,
@@ -286,7 +289,7 @@ class _ArcaconListPage extends State<ArcaconPage> {
                                                 .controller
                                                 .onPlayerVisibilityChanged(
                                                     info.visibleFraction);
-                                            */ /*if (info.visibleFraction == 0) {
+                                            */ /* */ /*if (info.visibleFraction == 0) {
                                             videoControllers[position]!
                                                 .player
                                                 .controller
@@ -304,83 +307,85 @@ class _ArcaconListPage extends State<ArcaconPage> {
                                               videoControllers[position]!
                                                   .isPlaying = true;
                                             }
-                                          }*/ /*
-                                          });*/
-                                    }
+                                          }*/ /* */ /*
+                                          });*/ /*
+                                      }
 
-                                    print('영상 없음');
-                                    return const CircularProgressIndicator();
+                                      print('영상 없음');
+                                      return const CircularProgressIndicator();
+                                    },
+                                  ),*/
+                                        CircularProgressIndicator())
+                              else
+                                Image.network(
+                                  snapshot.data![position].imageUrl,
+                                  width: 100,
+                                  height: 100,
+                                  loadingBuilder: (BuildContext context,
+                                      Widget child,
+                                      ImageChunkEvent? loadingProgress) {
+                                    if (loadingProgress == null) {
+                                      return child;
+                                    }
+                                    return Center(
+                                      child: SizedBox(
+                                        width: 100,
+                                        height: 100,
+                                        child: CircularProgressIndicator(
+                                          value: loadingProgress
+                                                      .expectedTotalBytes !=
+                                                  null
+                                              ? loadingProgress
+                                                      .cumulativeBytesLoaded /
+                                                  loadingProgress
+                                                      .expectedTotalBytes!
+                                              : null,
+                                        ),
+                                      ),
+                                    );
                                   },
                                 ),
-                              )
-                            else
-                              Image.network(
-                                snapshot.data![position].imageUrl,
-                                width: 100,
-                                height: 100,
-                                loadingBuilder: (BuildContext context,
-                                    Widget child,
-                                    ImageChunkEvent? loadingProgress) {
-                                  if (loadingProgress == null) {
-                                    return child;
-                                  }
-                                  return Center(
-                                    child: SizedBox(
-                                      width: 100,
-                                      height: 100,
-                                      child: CircularProgressIndicator(
-                                        value: loadingProgress
-                                                    .expectedTotalBytes !=
-                                                null
-                                            ? loadingProgress
-                                                    .cumulativeBytesLoaded /
-                                                loadingProgress
-                                                    .expectedTotalBytes!
-                                            : null,
-                                      ),
-                                    ),
-                                  );
-                                },
+                              const SizedBox(
+                                height: 10.0,
                               ),
-                            const SizedBox(
-                              height: 10.0,
-                            ),
-                            Container(
-                              child: Text(
-                                snapshot.data![position].title,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                                overflow: TextOverflow.ellipsis,
+                              Container(
+                                child: Text(
+                                  snapshot.data![position].title,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
                               ),
-                              margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                            ),
-                            const SizedBox(
-                              height: 5.0,
-                            ),
-                            Container(
-                              child: Text(
-                                snapshot.data![position].maker,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.normal),
-                                overflow: TextOverflow.ellipsis,
+                              const SizedBox(
+                                height: 5.0,
                               ),
-                              margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                            ),
-                          ],
+                              Container(
+                                child: Text(
+                                  snapshot.data![position].maker,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.normal),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6)),
-                    );
-                  },
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  itemCount: snapshot.data!.length,
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 150,
-                    //crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-                    childAspectRatio: 1 / 1.4, //item 의 가로 1, 세로 2 의 비율
-                    mainAxisSpacing: 10, //수평 Padding
-                    crossAxisSpacing: 10, //수직 Padding
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6)),
+                      );
+                    },
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: snapshot.data!.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 150,
+                      //crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+                      childAspectRatio: 1 / 1.4, //item 의 가로 1, 세로 2 의 비율
+                      mainAxisSpacing: 10, //수평 Padding
+                      crossAxisSpacing: 10, //수직 Padding
+                    ),
                   ),
                 ),
               );
