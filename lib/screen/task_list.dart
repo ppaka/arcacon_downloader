ef612f1fcc8cbcd959d342808f850f4520aa0a41
ppaka <asj0216@gmail.com>
2023-03-23 20:36:25 +0900

    페이지 생성

---
+++ lib/screen/task_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskList extends StatefulWidget {
+  const TaskList({Key? key}) : super(key: key);
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
