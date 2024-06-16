1497f3458a86aa3b49f40832e2e25be450f3f6c0
ppaka <asj0216@gmail.com>
2024-06-17 01:23:36 +0900

    코드 재작성 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,338 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.white,
+    skipTaskbar: false,
+    title: "아카콘 다운로더",
+    titleBarStyle: TitleBarStyle.normal,
+  );
+
+  await windowManager.setMaximizable(false);
+  await windowManager.setMinimizable(true);
+  await windowManager.setResizable(false);
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(const AppRoot());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.unknown
+      };
+}
 
-  // This widget is the root of your application.
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Arcacon Downloader',
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.dark,
+      home: const MainPage(title: '아카콘 다운로더'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Arcacon {
+  final int id;
+  final String imageUrl;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Arcacon({required this.id, required this.imageUrl});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  factory Arcacon.fromJson(Map<String, dynamic> json) {
+    return Arcacon(
+      id: json['id'] as int,
+      imageUrl: 'https:${json['imageUrl']}',
+    );
+  }
+}
 
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ArcaconList extends StatelessWidget {
+  const ArcaconList(
+      {super.key, required this.arcacons, required this.controller});
+  final List<Arcacon> arcacons;
+  final ScrollController controller;
 
-  void _incrementCounter() {
+  @override
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+      behavior: MyCustomScrollBehavior(),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 7,
+          childAspectRatio: 1,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        controller: controller,
+        itemCount: arcacons.length,
+        itemBuilder: (context, index) {
+          var arcacon = arcacons[index];
+          return CachedNetworkImage(
+            imageUrl: arcacon.imageUrl,
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                CircularProgressIndicator(value: downloadProgress.progress),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  Future<List<Arcacon>> fetchArcacon(String shopPageUrl) async {
+    Client client = Client();
+
+    var arcaconShopId = shopPageUrl.split('/').last.split('?').first;
+    if (int.tryParse(arcaconShopId) == null) {
+      return [];
+    }
+
+    final response = await client.get(
+      Uri.parse("https://arca.live/api/app/list/emoticon/$arcaconShopId"),
+      headers: {
+        "User-Agent": "live.arca.android/0.8.369",
+        "Host": "arca.live",
+        "X-Device-Token": "",
+      },
+    );
+    // return await compute(parseArcacon, response.body);
+    return parseArcacon(response.body);
+  }
+
+  List<Arcacon> parseArcacon(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Arcacon>((json) => Arcacon.fromJson(json)).toList();
+  }
+
+  void downloadArcacons(String shopPageUrl) async {
+    var arcaconShopId = shopPageUrl.split('/').last.split('?').first;
+    var dio = Dio();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      taskPercent = Future.value(-1);
     });
+    final arcacons = await fetchArcacon(shopPageUrl);
+    final lengthChrCount = arcacons.length.toString().length;
+    var downloadsDirectory = await getDownloadsDirectory();
+    var directoryStr = "${downloadsDirectory!.path}/$arcaconShopId";
+    var directory = await Directory(directoryStr).create();
+
+    setState(() {
+      taskPercent = Future.value(0);
+    });
+
+    for (int i = 0; i < arcacons.length; i++) {
+      var con = arcacons[i];
+      var fileExtension = con.imageUrl.split('?').first.split('.').last;
+      var filename =
+          "${(i + 1).toString().padLeft(lengthChrCount, '0')}.$fileExtension";
+      await dio.download(con.imageUrl, "${directory.path}/$filename");
+
+      setState(() {
+        taskPercent = Future.value(i + 1 / arcacons.length);
+      });
+    }
+
+    setState(() {
+      taskPercent = Future.value(null);
+    });
+  }
+
+  late Future<List<Arcacon>> myFuture;
+  late TextEditingController textEditingController = TextEditingController();
+  late ScrollController controller = ScrollController();
+  late Future<double?> taskPercent = Future.value(null);
+
+  Widget singleTaskButton() {
+    return FutureBuilder<double?>(
+      future: taskPercent,
+      builder: (context, snapshot) {
+        if (snapshot.data == null) {
+          return SizedBox(
+            width: 800,
+            height: 36,
+            child: FilledButton(
+              onPressed: () {
+                downloadArcacons(textEditingController.text);
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('다운로드'),
+            ),
+          );
+        } else {
+          if (snapshot.data! < 0) {
+            return LinearProgressIndicator(
+              borderRadius: BorderRadius.circular(10),
+              minHeight: 36,
+              value: null,
+            );
+          }
+          return LinearProgressIndicator(
+            borderRadius: BorderRadius.circular(10),
+            minHeight: 36,
+            value: snapshot.data,
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    myFuture = fetchArcacon('');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: Colors.black,
+            width: 800,
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                Flexible(
+                  child: TextField(
+                    controller: textEditingController,
+                    textInputAction: TextInputAction.next,
+                    decoration: const InputDecoration(
+                      labelText: '아카콘 주소',
+                      hintText: '상점 페이지 URL을 입력해주세요',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.url,
+                    onSubmitted: (value) {
+                      setState(() {
+                        myFuture = fetchArcacon(value);
+                        controller.position.moveTo(
+                          0,
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.fastOutSlowIn,
+                        );
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  child: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        myFuture = fetchArcacon(textEditingController.text);
+                        controller.position.moveTo(
+                          0,
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.fastOutSlowIn,
+                        );
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Container(
+            width: 800,
+            height: 600 - 160,
+            color: Colors.white12,
+            child: FutureBuilder<List<Arcacon>>(
+              future: myFuture,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  if (kDebugMode) {
+                    print(snapshot.error);
+                  }
+                }
+                if (snapshot.data != null) {
+                  if (snapshot.hasData) {
+                    return ArcaconList(
+                      arcacons: snapshot.data!,
+                      controller: controller,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                } else {
+                  return Center(
+                    child: Text(
+                      snapshot.error.toString(),
+                    ),
+                  );
+                }
+              },
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Colors.transparent,
+            width: 800,
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            alignment: Alignment.center,
+            child: singleTaskButton(),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
